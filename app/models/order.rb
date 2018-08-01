59c542853d2f82151c642fe2b97b20dc50141e6b
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-01 15:33:33 +0000

    added db seeds

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ApplicationRecord
+  has_many :items
+  has_many :products, through: :items
+
+  def order_total
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create(name: "Mickey", password: "mouse", email: "mickey@gmail.com")
+product1 = Product.create(name: "Mickey's Magic Gloves", price: 10.99, inventory: 100)
+order1 = Order.create(user_id: 1)
+item1 = Item.create(user_id: 1, product_id: 1, order_id: 1)
