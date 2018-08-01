a0af453df0950fca9b25ed7130aa9ac3813aa3b5
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-01 15:21:02 +0000

    added validations for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :price, numericality: {greater_than: 0}
+  validates :price, presence: true
+  validates :inventory, presence: true
 end
