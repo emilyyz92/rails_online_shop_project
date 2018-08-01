13af6e5bf58e21cedf617967a9b6f21c09c652ce
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-01 17:32:50 +0000

    updated user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone_number, length: {is: 10}
   has_many :items
   has_many :orders
   has_many :products, through: :items
--- db/migrate/20180801135626_create_users.rb
+++ db/migrate/20180801135626_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.string :email
+      t.string :phone_number
       t.string :password_digest
       t.boolean :admin, default: false
 
